75fa98ea08381a94c0207c771b58aaaec0ccc47b
chck <iwazaki_yuki@cyberagent.co.jp>
2015-02-23 05:11:07 +0900

    モデルにRelationShipを追加

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -2,4 +2,5 @@ require 'active_record'
 
 ActiveRecord::Base.establish_connection(ENV['DATABASE_URL']||'sqlite3:db/development.db')
 class Follow < ActiveRecord::Base
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ require 'active_record'
 
 ActiveRecord::Base.establish_connection(ENV['DATABASE_URL']||'sqlite3:db/development.db')
 class User < ActiveRecord::Base
+  has_many :follows, dependent: :destroy
 end
